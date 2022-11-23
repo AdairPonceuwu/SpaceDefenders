5e7af1763f129df6273339b797646bc47398fe12
boribs <christumer@gmail.com>
2022-11-23 14:28:50 -0600

    Funciones para escalar y rotar cómodamente

--- entity.h
+++ entity.h
@@ -10,7 +10,7 @@ public:
           velocidad,
           angulo,
           V[3] = { 0 },
-          S[3] = { 0 },
+          S[3] = { 1 },
           R[3] = { 0 };
     bool vivo = true;
     Object *obj;
@@ -23,6 +23,17 @@ public:
     }
     virtual void mata() {
         vivo = false;
+    }
+    virtual void rotate(float angle, float x, float y, float z) {
+        angulo = angle;
+        R[0] = x;
+        R[1] = y;
+        R[2] = z;
+    }
+    virtual void scale(float f) {
+        S[0] = f;
+        S[1] = f;
+        S[2] = f;
     }
 };
 
